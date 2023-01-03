03374d174049ceabab5851b5af09617c07af42e8
we1lzst <shitreshing@gmail.com>
2023-01-03 16:47:15 +0900

    master

---
+++ script.js
@@ -0,0 +1,9 @@
+function sign() {
+    let block = document.querySelector('.header-login').children[0];
+    console.log(block)
+    block.addEventListener('click', () => {
+        alert("я не сделал форму((((")
+
+    })
+}
+sign();
